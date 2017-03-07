709ad63e9d31871fa99bd26e6fe872fdcb631108
Russell Williams <russellwilliamscc@gmail.com>
2017-03-07 15:54:36 +0000

    changes population string into integer

--- app/planet-list/planet-list.component.js
+++ app/planet-list/planet-list.component.js
@@ -10,6 +10,7 @@ component('planetList', {
     var self = this;
     self.orderProp = 'index';
 
+
     self.changeOrder = function(x){
       self.orderProp = x;
       console.log("Hello")
@@ -17,9 +18,19 @@ component('planetList', {
 
     $http.get('http://swapi.co/api/planets/').then(function(response) {
       self.planets = response.data.results;
-      // console.log(self.planets)
+      return self.planets
+    }).then(function(planets){
+      
+      for ( var i = 0; i < 10; i++ ){
+        if (self.planets[i].population !== "unknown"){
+       self.planets[i].population = (parseInt(planets[i].population))
+      }
+    }
     });
 
+       
+
+    
 
     // $http.get($routeParams.planetId.films[0]).then(function(response) {
     //   console.log("do this worketh now?", $routeParams.planetId)
